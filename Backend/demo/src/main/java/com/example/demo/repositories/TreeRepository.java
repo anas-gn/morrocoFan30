8e18e5d47b2d002c0e93134e187b28cd513a3775
ABDELLATIF1936 <abdellatifharakat2004@gmail.com>
2026-01-25 12:01:45 +0100

    Sauvegarde

--- Backend/demo/src/main/java/com/example/demo/repositories/TreeRepository.java
+++ Backend/demo/src/main/java/com/example/demo/repositories/TreeRepository.java
@@ -1,1 +1,11 @@
 package com.example.demo.repositories;
+
+import com.example.demo.models.Tree;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TreeRepository extends JpaRepository<Tree, Integer> {
+    List<Tree> findByType(String type);
+}
