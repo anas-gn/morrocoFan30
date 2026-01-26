bcf132a668e824be31c6dcdd24f2f671132f39ae
ragnar <anasgana2003@gmail.com>
2026-01-26 16:41:46 +0100

    heil

--- Backend/demo/src/main/java/com/example/demo/repositories/MatchRepository.java
+++ Backend/demo/src/main/java/com/example/demo/repositories/MatchRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.models.Match;
 
@@ -22,6 +22,6 @@ public interface MatchRepository extends JpaRepository<Match, Integer> {
                     )
                 )
             """)
-    List<Match> findMatchesByGroupeId(int id);
+    List<Match> findMatchesByGroupeId(@Param("groupeId")int groupeId);
 
 }
--- Backend/demo/src/main/java/com/example/demo/repositories/MatchTeamRepository.java
+++ Backend/demo/src/main/java/com/example/demo/repositories/MatchTeamRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.models.MatchTeam;
 
@@ -12,11 +13,11 @@ public interface MatchTeamRepository extends JpaRepository<MatchTeam, Integer> {
                    SELECT mt FROM MatchTeam mt where mt.matchID = :id
 
                """)
-     List<MatchTeam> findByMatchId(int id);
+     List<MatchTeam> findByMatchId(@Param("id")int id);
 
      @Query("""
                    SELECT mt FROM MatchTeam mt where mt.matchID = :id and mt.teamID = :idT
 
                """)
-     MatchTeam findByMatchIdAndTeamId(int id, int idT);
+     MatchTeam findByMatchIdAndTeamId(@Param("id")int id,@Param("idT") int idT);
 }
