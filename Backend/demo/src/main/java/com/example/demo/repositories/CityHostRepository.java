55e6e1b42686894cced3f09318d82e9646a01261
pahae-tests <wahipahae@gmail.com>
2026-01-27 12:15:59 +0100

    Delete Backend/demo/src/main/java/com/example/demo/repositories directory

--- Backend/demo/src/main/java/com/example/demo/repositories/CityHostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.CityHost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityHostRepository extends JpaRepository<CityHost, Long> {
-}
--- Backend/demo/src/main/java/com/example/demo/repositories/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.models.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-   
-}
