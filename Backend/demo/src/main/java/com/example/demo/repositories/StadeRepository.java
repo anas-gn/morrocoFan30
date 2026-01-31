3dd5e3e4d31a2eb6f94767c843efd42cae31f786
ABDELLATIF1936 <abdellatifharakat2004@gmail.com>
2026-01-31 23:33:01 +0100

    stade

--- Backend/demo/src/main/java/com/example/demo/repositories/StadeRepository.java
+++ Backend/demo/src/main/java/com/example/demo/repositories/StadeRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repositories;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.models.Stades;
+import org.springframework.data.domain.Pageable;
 
 public interface StadeRepository extends JpaRepository<Stades, Integer> {
 
@@ -12,5 +13,5 @@ public interface StadeRepository extends JpaRepository<Stades, Integer> {
 
     List<Stades> findByCityHostId(int cityHostId);
 
-    List<Stades> findTopByOrderByCapacityDesc(int nombre);
+   List<Stades> findAllByOrderByCapacityDesc(Pageable pageable);
 }
